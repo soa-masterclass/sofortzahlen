ad056177fc5330f7001d8b5875d53d2bd5e627b3
Administrator <Administrator@172.31.22.178>
2014-04-03 12:57:28 +0200

    UseTest EJB

---
+++ src/main/java/com/cgi/soa/masterclass/sofortzahlen/service/UserTest.java
@@ -0,0 +1,43 @@
+package com.cgi.soa.masterclass.sofortzahlen.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.cgi.soa.masterclass.sofortzahlen.model.User;
+
+@Stateless 
+public class UserTest {
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(User user) {
+		em.persist(user);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<User> selectAllUser(EntityManager em)
+	{
+		 
+		List<User> users = new LinkedList<User>();
+		try
+		{
+			String query = "select q from from User q";
+			Query q = em.createQuery(query);
+			users = (List<User>) q.getResultList();	
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("Fehler beim Select Alle Users");
+		}
+		return users;
+		
+	}
+
+	
+}
