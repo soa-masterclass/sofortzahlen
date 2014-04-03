df3ee44e7c21c0a02ecc48cdede7f1757fbe52f4
Administrator <Administrator@172.31.22.178>
2014-04-03 09:59:12 +0200

    test User zum Auffüllen

---
+++ src/main/java/com/cgi/soa/masterclass/sofortzahlen/model/User.java
@@ -0,0 +1,23 @@
+package com.cgi.soa.masterclass.sofortzahlen.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public User() {
+		super();
+	}
+	
+   
+}
