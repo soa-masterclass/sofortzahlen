cbb95ba0fbda1904cd1caada939cb9fa1330f9ee
Administrator <Administrator@172.31.19.35>
2014-04-03 11:03:58 +0200

    Managed Beans initial Version

--- src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/UserManagedBean.java
+++ src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/UserManagedBean.java
@@ -1,10 +1,14 @@
 package com.cgi.soa.masterclass.sofortzahlen.faces;
 
+import java.util.ArrayList;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+
+import com.cgi.soa.masterclass.sofortzahlen.model.User;
 
 @Named
 @RequestScoped
 public class UserManagedBean {
-
+	ArrayList<User> userList = new ArrayList<User>();
 }
