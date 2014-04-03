8fd333f29598bfe8c64b12733109b02ddbfa9a51
Administrator <Administrator@172.31.19.35>
2014-04-03 13:37:59 +0200

    UserManagedBean aktualisiert

--- src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/UserManagedBean.java
+++ src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/UserManagedBean.java
@@ -1,20 +1,26 @@
 package com.cgi.soa.masterclass.sofortzahlen.faces;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.cgi.soa.masterclass.sofortzahlen.model.User;
+import com.cgi.soa.masterclass.sofortzahlen.service.UserEJB;
 
 @Named
 @RequestScoped
 public class UserManagedBean {
 	ArrayList<User> userList = new ArrayList<User>();
 	
+	@Inject
+	UserEJB userEJB;
+	
 
-	public ArrayList<User> getUserList() {
-		return createSampleUser();
+	public List<User> getUserList() {
+		return userEJB.selectAllUser();
 	}
 
 
@@ -23,20 +29,28 @@ public class UserManagedBean {
 		this.userList = userList;
 	}
 
-	private ArrayList<User> createSampleUser(){
-		User user = new User();
-		user.setId(1234);
-		user.setEmail("a@a.com");
-		user.setFirstname("Max");
-		user.setLastname("Mustermann");
-		user.setAccnumber(1223);
-		user.setAccfirstname("Max");
-		user.setAcclastname("Mustermann");
-		user.setBalance(1000L);
-		userList.clear();
-		userList.add(user);
-		return userList;
+	public User getUser(){
+		return null;		
 	}
+	
+//	public String createUser(){
+//		
+//	}
+	
+//	private ArrayList<User> createSampleUser(){
+//		User user = new User();
+//		user.setId(1234);
+//		user.setEmail("a@a.com");
+//		user.setFirstname("Max");
+//		user.setLastname("Mustermann");
+//		user.setAccnumber(1223);
+//		user.setAccfirstname("Max");
+//		user.setAcclastname("Mustermann");
+//		user.setBalance(1000L);
+//		userList.clear();
+//		userList.add(user);
+//		return userList;
+//	}
 	
 	
 }
