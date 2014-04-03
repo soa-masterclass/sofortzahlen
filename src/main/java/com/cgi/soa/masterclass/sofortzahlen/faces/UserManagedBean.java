c274daaaebfcb507a3b92304f215176e55f3470a
Administrator <Administrator@172.31.19.35>
2014-04-03 12:08:07 +0200

    Index Version 2.0

--- src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/UserManagedBean.java
+++ src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/UserManagedBean.java
@@ -11,4 +11,32 @@ import com.cgi.soa.masterclass.sofortzahlen.model.User;
 @RequestScoped
 public class UserManagedBean {
 	ArrayList<User> userList = new ArrayList<User>();
+	
+
+	public ArrayList<User> getUserList() {
+		return createSampleUser();
+	}
+
+
+	public void setUserList(ArrayList<User> userList) {
+		
+		this.userList = userList;
+	}
+
+	private ArrayList<User> createSampleUser(){
+		User user = new User();
+		user.setId(1234);
+		user.setEmail("a@a.com");
+		user.setFirstname("Max");
+		user.setLastname("Mustermann");
+		user.setAccnumber(1223);
+		user.setAccfirstname("Max");
+		user.setAcclastname("Mustermann");
+		user.setBalance(1000L);
+		userList.clear();
+		userList.add(user);
+		return userList;
+	}
+	
+	
 }
