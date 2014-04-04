ddbf98e3fd1435231fccc47e0b2a113c5d1f1d55
Administrator <Administrator@172.31.22.178>
2014-04-04 09:07:23 +0200

    pay deposit,-Pay,-Clearing-ManagedEJB (Not yet emplemented-- still comming)

---
+++ src/main/java/com/cgi/soa/masterclass/sofortzahlen/faces/ClearingManagedBean.java
@@ -0,0 +1,11 @@
+package com.cgi.soa.masterclass.sofortzahlen.faces;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class ClearingManagedBean 
+{
+
+}
